c5f0dc62e7a68f7d2a7179041dd5c376e3d00388
Graham Smith <gsmith1@hssports.co.uk>
2017-01-17 15:50:02 +0000

    Setup app for Heroku2

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,5 @@
 var mongoose = require('mongoose');
 
-const
-
 mongoose.Promise = global.Promise;
 mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/TodoApp2');
 module.exports = {
