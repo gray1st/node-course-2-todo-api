f1446a6dfdce6e76a0c0b1b1358db24930beca5f
Graham Smith <gsmith1@hssports.co.uk>
2017-01-17 15:51:33 +0000

    Setup app for Heroku3

--- server/db/mongoose.js
+++ server/db/mongoose.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.Promise = global.Promise;
-mongoose.connect(process.env.MONGO_URI || 'mongodb://localhost:27017/TodoApp2');
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TodoApp2');
 module.exports = {
   mongoose
 };
