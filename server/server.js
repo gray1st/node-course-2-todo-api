e9982f42ffcef78d51e484bd05d7da8b0bb82bcd
Graham Smith <gsmith1@hssports.co.uk>
2017-01-18 10:06:20 +0000

    Added Patch Todo

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 const bodyParser = require('body-parser');
 const express = require('express');
 const {ObjectID} = require('mongodb');
+const _ = require('lodash');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -61,6 +62,31 @@ app.delete('/todos/:id', (req, res)=>{
     res.status(400).send();
   });
 });
+
+app.patch('/todos/:id',(req,res)=>{
+  //Using LoDash create an object with the following data if it exists
+  var body = _.pick(req.body,['text', 'completed']);
+
+  if (!ObjectID.isValid(req.params.id)){
+      return res.status(404).send();
+  };
+  if (_.isBoolean(body.completed) && body.completed){
+    body.completedAt = new Date().getTime();
+  } else {
+    body.completed = false;
+    body.completedAt = null;
+  }
+
+  Todo.findByIdAndUpdate(req.params.id, {$set: body}, {new: true}).then((todo) =>{
+    if (!todo){
+      return res.status(404).send();
+    }
+    res.status(200).send({todo});
+  }).catch((e) =>{
+    return res.status(404).send();
+  })
+});
+
 
 app.listen(port, () => {
   console.log('Server Started on Port' + port);
